17e5ce38671bc21c1246b86e51d34b1712fccce7
Dominic-Madeira <dominic.madeira@mths.ca>
2024-12-11 14:38:39 +0000

    pushed changes

---
+++ ts/index.ts
@@ -0,0 +1,12 @@
+/**
+ * This program is the Hello, World! program.
+ *
+ * By:      Dominic M.
+ * Version: 1.0
+ * Since    2024-12-11
+ */
+
+// print Hello, World!
+console.log('Hello, World!')
+
+console.log('\nDone.')
